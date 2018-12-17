94ed619051a05b6599d482ee33aa90bd308e6d53
Yuki Hirayama <hirayama.yuki@his-world.com>
2018-12-17 17:58:48 +0900

    modify modal-button.js

--- src/components/modal-button.js
+++ src/components/modal-button.js
@@ -1,6 +1,34 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
 class ModalButton extends PolymerElement {
+    static get properties() {
+        return {
+            clicked: {
+                type: Boolean,
+                value: false,
+                notify: true,
+                reflectToAttribute: true
+            },
+            buttonText: {
+                type: String
+            },
+            buttonType: {
+                type: String
+            },
+            images: {
+                type: Array,
+                value() {
+                    return [
+                        { src: 'https://www.his-j.com/smp/test/images/ex01.jpg' },
+                        { src: 'https://www.his-j.com/smp/test/images/Namib_Desert01.jpg' },
+                        { src: 'https://www.his-j.com/smp/test/images/Namib_Desert02.jpg' },
+                        { src: 'https://www.his-j.com/smp/test/images/Namib_Desert03.jpg' },
+                        { src: 'https://www.his-j.com/smp/test/images/Namib_Desert04.jpg' }
+                    ];
+                }
+            }
+        };
+    }
     static get template() {
         return html`
             <style>
@@ -25,40 +53,33 @@ class ModalButton extends PolymerElement {
                 background-color: #666;
                 color: #fff;
                 }
+                .modal-conts {
+                    z-index: 2;
+                }
             </style>
-            <button type$="[[buttonType]]" name="button" on-click="handleClick">[[buttonText]]
-                <slot></slot>
-            </button>
+            <div id="modal-box">
+                <button type$="[[buttonType]]" name="button" on-click="openModal">[[buttonText]]
+                    <slot></slot>
+                </button>
+            </div>
         `;
-    }
-    static get properties() {
-        return {
-            clicked: {
-                type: Boolean,
-                value: false,
-                notify: true,
-                reflectToAttribute: true
-            },
-            buttonText: {
-                type: String
-            },
-            buttonType: {
-                type: String
-            },
-        };
     }
     constructor() {
         super();
-        //this.addEventListener('click', this.modal.bind(this));
     }
-    // modal() {
-    //     this.clicked = !this.clicked;
-    // }
-    handleClick() {
-        const div = document.createElement("div");
-        const text = document.createTextNode("こんにちは!");
-        div.appendChild(text);
-        this.appendChild(div);
+    openModal() {
+        this.clicked = !this.clicked;
+        const modal_conts = document.createElement("div");
+        modal_conts.id = "modal-conts"
+        const modal_img = document.createElement("img");
+        modal_img.src = "https://www.his-j.com/smp/test/images/Namib_Desert01.jpg";
+        modal_conts.appendChild(modal_img);
+        if (this.clicked) {
+            document.body.appendChild(modal_conts);
+        } else {
+            const new_modal = document.getElementById('modal-conts');
+            new_modal.parentNode.removeChild(new_modal);
+        }
     }
 }
 customElements.define('modal-button', ModalButton);
